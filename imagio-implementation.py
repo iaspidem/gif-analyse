88cd962f8a2a7682b3d3bb2abecc5c514709dbbe
JPR <iaspis.st@gmail.com>
2025-11-19 22:32:12 +0000

    added initial imagio implementation

--- imagio-implementation.py
+++ imagio-implementation.py
@@ -1,2 +1,11 @@
 #import module
 import imageio
+
+gif = imageio.get_reader("data/delorean-retroloop.gif")
+
+duration = 0
+
+for frame in gif:
+    duration += gif.get_meta_data(index=frame)["duration"]
+
+print(f"Total duration: {duration} ms")
