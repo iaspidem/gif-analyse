2235ea9f2a3967a2a1c90fd8c77b212329520a93
JPR <iaspis.st@gmail.com>
2025-11-19 20:46:04 +0000

    added initial pillow implementation

--- pillow-implementation.py
+++ pillow-implementation.py
@@ -1,2 +1,16 @@
 # import pillow
 from PIL import Image
+
+img = Image.open("data/Steam-SunsetCruise/Artwork_Featured.gif")
+
+duration = 0
+
+while True:
+    try:
+        frame_duration = img.info['duration']
+        duration += frame_duration
+        img.seek(img.tell() + 1)
+    except EOFError:
+        break
+
+print(f"Total duration: {duration} ms")
