67c5215e8040f5e96505923a05c93ecbf51b3613
JPR <iaspis.st@gmail.com>
2025-11-20 00:16:42 +0000

    update verify-lib.py

--- verify-lib.py
+++ verify-lib.py
@@ -1,9 +1,6 @@
+import os
 from PIL import Image
 import imageio
-
-def read_input():
-
-    return
 
 def pillow_fn(img):
     duration = 0
@@ -25,3 +22,22 @@ def imageio_fn(gif):
         duration += gif.get_meta_data(index=frame)["duration"]
 
     return duration
+
+def verify_fn(dir):
+    for path, folders, files in os.walk(dir):
+        # Open file
+        for filename in files:
+            print(f"{filename}")
+            #print(f"Filepath: {filepath}")
+
+        for folder_name in folders:
+            print(f"Content of '{folder_name}'")
+            # List content from folder
+            print(os.listdir(f"{path}/{folder_name}"))
+            print()
+    #break   
+
+# Assign directory
+dir = r"data"
+
+verify_fn(dir)
