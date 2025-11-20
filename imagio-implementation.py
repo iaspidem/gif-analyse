4ef01233d90e59f31d385736aeaf662aa75b41c1
JPR <iaspis.st@gmail.com>
2025-11-20 01:36:00 +0000

    implemented frames per second computation

--- imagio-implementation.py
+++ imagio-implementation.py
@@ -4,8 +4,13 @@ import imageio
 gif = imageio.get_reader("data/delorean-retroloop.gif")
 
 duration = 0
+frame_counter = 0
 
 for frame in gif:
+    frame_counter +=1
     duration += gif.get_meta_data(index=frame)["duration"]
+    fps = frame_counter / (duration / 1000)
 
 print(f"Total duration: {duration} ms")
+print(f"Total number of frames: {frame_counter} frames")
+print(f"Frames per second: {fps} fps")
