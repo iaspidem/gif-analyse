d58b970d665ed9572daa61e66696ac58bb1227ad
JPR <iaspis.st@gmail.com>
2025-11-22 00:37:33 +0000

    added started code for saving frames

--- frame-visualiser.py
+++ frame-visualiser.py
@@ -11,3 +11,12 @@ print(f"File size: {int(round(file_size / 1024, 0))} KB ({round(file_size / pow(
 
 img = Image.open(filepath)
 print("Number of frames (n_frames): " + str(img.n_frames))
+
+#img_start = img.seek(0)
+#img_start.save("output/img_start.png")
+
+#frames = []
+#frames.append(img.seek(0))
+#frames.append(img.seek(img.n_frames - 1))
+
+#frames[0].save("visualiser-output.gif", save_all=True, append_images=frames[1:], duration=100, loop=0)
